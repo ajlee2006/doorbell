1f0149045bbb75567f3ea26125537e659ffcb1fa
Andrew Lee <ajlee2006@gmail.com>
2021-06-10 16:29:52 +0800

    fixed py

--- doorbellserial.py
+++ doorbellserial.py
@@ -1,10 +1,12 @@
-import serial, sys, time, os
+import serial, os, random
 
 ser = serial.Serial('/dev/ttyUSB0',9600)
 ser.flushInput()
+
+ringtones = ['doorbell-twice.wav','eine-kleine.wav']
 
 while True:
     msg = ser.read(ser.inWaiting()).decode()
     if msg != '':
         print("Ding dong")
-        os.system("aplay doorbell-1.wav")
+        os.system("aplay " + random.choice(ringtones))
