16fe8ce593e9d57b834dc8441351b97660c03351
Andrew Lee <ajlee2006@gmail.com>
2021-06-11 12:28:46 +0800

    .

--- doorbellserial.py
+++ doorbellserial.py
@@ -3,10 +3,11 @@ import serial, os, random
 ser = serial.Serial('/dev/ttyUSB0',9600)
 ser.flushInput()
 
-ringtones = os.listdir('ringtones')
+#ringtones = os.listdir('ringtones')
 
 while True:
     msg = ser.read(ser.inWaiting()).decode()
     if msg != '':
         print("Ding dong")
-        os.system("aplay ringtones/" + random.choice(ringtones))
+        #os.system("aplay ringtones/" + random.choice(ringtones))
+        os.system("aplay ringtones/doorbell-twice.wav")
