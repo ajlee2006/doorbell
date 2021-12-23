6c902e4935781c86d513aa1edaa9b2ebb030f32d
Andrew Lee <ajlee2006@gmail.com>
2021-12-23 22:42:53 +0800

    Update doorbellarduino.ino

--- doorbellarduino.ino
+++ doorbellarduino.ino
@@ -2,6 +2,11 @@ const int buttonPin = 2;
 const int ledPin = 13;
 int buttonState = 0;
 int oldButtonState = 0;
+int change = 0;
+int timerOn = 0;
+int ctr = 0;
+int minCtr = 2;
+int zCtr = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -12,9 +17,37 @@ void setup() {
 void loop() {
   oldButtonState = buttonState;
   buttonState = digitalRead(buttonPin);
-  
-  if (buttonState == HIGH) {
+
+  if (oldButtonState == LOW && buttonState == HIGH)
+    change = 1;
+  else if (oldButtonState == HIGH && buttonState == LOW)
+    change = -1;
+  else
+    change = 0;
+
+  if (change == 1){
+    timerOn = 1;
     digitalWrite(ledPin, HIGH);
-  } Serial.println(buttonState); delay(100);
-}
+  }
+  else if (change == -1) {
+    timerOn = 0;
+    digitalWrite(ledPin, LOW);
+    if (ctr > minCtr) {
+      Serial.println(ctr);
+    } 
+  }
+
+  if (timerOn == 1)
+    ctr++;
+  else {
+    ctr = 0;
+  }
   
+  delay(50);
+  /*
+  zCtr++;
+  if (zCtr % 2 == 0){
+    Serial.println(0);
+    zCtr = 0;
+  }*/
+}
