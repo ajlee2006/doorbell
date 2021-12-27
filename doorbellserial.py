c9bb59fd11c69adc696d95f5fe5c211fd5bf6c54
Andrew Lee <ajlee2006@gmail.com>
2021-12-27 20:29:07 +0800

    Update doorbellserial.py

--- doorbellserial.py
+++ doorbellserial.py
@@ -1,4 +1,4 @@
-import serial, os, random
+import serial, os, random, time, sys
 
 ser = serial.Serial('/dev/ttyUSB0',9600)
 ser.flushInput()
@@ -6,8 +6,20 @@ ser.flushInput()
 #ringtones = os.listdir('ringtones')
 
 while True:
-    msg = ser.read(ser.inWaiting()).decode()
-    if msg != '':
-        print("Ding dong")
-        #os.system("aplay ringtones/" + random.choice(ringtones))
-        os.system(random.choice(["aplay ringtones/doorbell-four.wav","aplay ringtones/doorbell-six.wav"]))
+    time.sleep(0.05)
+    try:
+        msgStr = ser.read(ser.inWaiting()).strip()
+        # print(msgStr, type(msgStr)) -> bytes
+        if len(msgStr) > 0:
+            msg = int(msgStr)
+            # print(msg)
+        else:
+            msg = -1
+        if msg > 0 :
+            print( "Ding dong", msg )
+            #os.system("aplay ringtones/" + random.choice(ringtones))
+            os.system(random.choice(["aplay ringtones/doorbell-four.wav","aplay ringtones/doorbell-six.wav"]))
+            print('completed')
+    except BaseException as err:
+        print("Error...", type(err))
+    sys.stdout.flush()
